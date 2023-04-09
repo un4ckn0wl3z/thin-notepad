58a2a1c12dbc33da3bee4396239514877d1c0f13
Anuwat <admin@unknownclub.net>
2023-04-09 22:36:02 +0700

    feat: add open and save feature

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from mainwindow import MainWindow
+# import qdarktheme
+
 
 app = QApplication(sys.argv)
+# qdarktheme.setup_theme()
+
 w = MainWindow(app)
 w.show()
 app.exec()
--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,5 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow,QMessageBox
+from PySide6.QtCore import Qt, QTextStream, QFile, QIODevice
+from PySide6.QtWidgets import QApplication, QMainWindow,QMessageBox, QFileDialog
 from PySide6.QtGui import QIcon
 from ui_mainwindow import Ui_MainWindow
 
@@ -8,7 +8,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.app = app
-        self.setWindowTitle("Thin Notepad v0.0.1")
+        self.setWindowTitle("Thin Notepad v0.0.2")
 
         appIcon = QIcon(":/images/book-open-list.png")
         self.setWindowIcon(appIcon)
@@ -21,6 +21,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.actionRedo.triggered.connect(self.redo)
         self.actionAbout.triggered.connect(self.about)
         self.actionAbout_Qt.triggered.connect(self.aboutQt)
+        self.actionSave.triggered.connect(self.write_triggered)
+        self.actionOpen.triggered.connect(self.open_triggered)
 
     def quit(self):
         self.app.quit()
@@ -38,4 +40,44 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         QMessageBox.information(self,"Going pro!","QMainWindow, Qt Designer and Resources : Going pro!")
     def aboutQt(self):
         QApplication.aboutQt()
+
+
+
+    def write_triggered(self):
+        file_name,_ = QFileDialog.getSaveFileName(self, "Save File",
+                                 ".",
+                                 "Text(*.txt);;Images (*.png *.xpm *.jpg);;All files(*.*)")
+        if((file_name == "")):
+            return
+        print("file :",file_name)
+        file = QFile(file_name)
+        if (not file.open(QIODevice.WriteOnly | QIODevice.Text)):
+            return
+        out = QTextStream(file)
+        out << self.textEdit.toPlainText() << "\n"
+        file.close()
+
+        self.statusBar().showMessage("File saved!", 5000)
+
+
+    def open_triggered(self):
+        file_content = ""
+        file_name,_ = QFileDialog.getOpenFileName(self, "Open File",
+                                 ".",
+                                 "Text(*.txt);;Images (*.png *.xpm *.jpg);;All files(*.*)")
+        if((file_name == "")):
+            return
+        print("Opened file :",file_name)
+        file = QFile(file_name)
+        if (not file.open(QIODevice.ReadOnly | QIODevice.Text)):
+            return
+        in_stream = QTextStream(file)
+        while (not in_stream.atEnd()) :
+            line = in_stream.readLine() + '\n'
+            file_content += line
+        file.close()
+        self.textEdit.clear()
+        self.textEdit.setText(file_content)
+
+        self.statusBar().showMessage("File openned!", 5000)
         
